18e5af67a03772f8d26e71048ac44131676b77a2
MarinaRasskazova <mari42004.mr@gmail.com>
2022-01-12 17:43:28 +0200

    Homework_01 completed

--- src/main/java/com/softserveinc/Task03.java
+++ src/main/java/com/softserveinc/Task03.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Task03 {
     public static void main(String[] args) {
 
-        int waterInMachine=Amount("Write how many ml of water the coffee machine has:");
-        int milkInMachine=Amount(" Write how many ml of milk the coffee machine has:");
-        int coffeeBeansInMachine=Amount("Write how many grams of coffee beans the coffee machine has:");
-        int cupsOfCoffeeNeed=Amount("Write how many cups of coffee you will need:");
+        int waterInMachine= amount("Write how many ml of water the coffee machine has:");
+        int milkInMachine= amount(" Write how many ml of milk the coffee machine has:");
+        int coffeeBeansInMachine= amount("Write how many grams of coffee beans the coffee machine has:");
+        int cupsOfCoffeeNeed= amount("Write how many cups of coffee you will need:");
 
-        int cupsEnoughWater=EnoughForCoups(waterInMachine, 200);
-        int cupsEnoughMilk=EnoughForCoups(milkInMachine, 50);
-        int cupsEnoughCoffeeBeans=EnoughForCoups(coffeeBeansInMachine, 15);
+        int cupsEnoughWater= enoughForCoups(waterInMachine, 200);
+        int cupsEnoughMilk= enoughForCoups(milkInMachine, 50);
+        int cupsEnoughCoffeeBeans= enoughForCoups(coffeeBeansInMachine, 15);
 
         int possibleAmountOfCups=cupsEnoughWater;
 
@@ -23,7 +23,6 @@ public class Task03 {
             if (possibleAmountOfCups>i){
                 possibleAmountOfCups=i;
             }
-            System.out.println("possibleAmountOfCups = " + possibleAmountOfCups);
         }
 
         if  (possibleAmountOfCups==cupsOfCoffeeNeed){
@@ -32,28 +31,26 @@ public class Task03 {
         if (possibleAmountOfCups<cupsOfCoffeeNeed){
             System.out.printf("No, I can make only %d cup(s) of coffee.", possibleAmountOfCups);
         }
-            if (possibleAmountOfCups>cupsOfCoffeeNeed){
+
+        if (possibleAmountOfCups>cupsOfCoffeeNeed){
                 System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)",(possibleAmountOfCups-cupsOfCoffeeNeed));
             }
-
-
         }
 
 
-
-    public static int Amount(String question){
+    public static int amount(String question){
         Scanner scanner = new Scanner(System.in);
         System.out.println(question);
         System.out.print(">>> ");
-        int amount = scanner.nextInt();
-        return amount;
+        int amountOfRaw = scanner.nextInt();
+        return amountOfRaw;
         }
 
-
-    public static int EnoughForCoups(int amountOfRaw, int consuption){
+    public static int enoughForCoups(int amountOfRaw, int consuption){
 
         int cups = amountOfRaw/consuption;
         return cups;
         }
+
     }
 
